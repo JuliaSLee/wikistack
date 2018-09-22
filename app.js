89c8c639f377619d11fb8700e4a0dc682d13fa21
Julia Lee <hopefullyea@gmail.com>
2018-09-22 11:52:40 -0500

    solution

--- app.js
+++ app.js
@@ -1,40 +1,18 @@
-const express = require('express')
-const morgan = require('morgan')
-const { db } = require("./models");
-const models = require("./models")
-const wikiRouter = require('./routes/wiki');
-const userRouter = require('./routes/user');
-
-
+const express = require("express");
 const app = express();
-const bodyParser = require("body-parser")
-const PORT = 3000;
+const morgan = require("morgan");
+const path = require("path");
 
-app.use(morgan("dev"));
-app.use(express.static(__dirname + "/public"))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use('/wiki', wikiRouter);
-app.use('/user', userRouter);
+app.use(morgan("dev")); //logging middleware
+app.use(express.static(path.join(__dirname, "./public"))); //serving up static files (e.g. css files)
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
+app.use("/wiki", require("./routes/wiki"));
+app.use("/users", require("./routes/users"));
 
-app.get('/', (req, res, next) => {
-    res.redirect('/wiki');
-})
+app.get('/', function (req, res) {
+   res.redirect('/wiki/');
+});
 
-db.authenticate().
-then(() => {
-    console.log('connected to the database');
-})
-
-const init = async () => {
-    await models.db.sync({force: true})
-    app.listen(PORT, () => {
-        console.log(`Server is listening on port ${PORT}!`);
-    });
-}
-
-init()
-
-
-
-
+module.exports = app;
--- routes/wiki.js
+++ routes/wiki.js
@@ -1,26 +1,98 @@
-const router = require("express").Router();
-const { Page } = require("../models");
-const { addPage } = require("../views")
+const express = require("express");
+const router = express.Router();
+const models = require("../models");
+const Page = models.Page;
+const User = models.User;
+const { main, addPage, editPage, wikiPage } = require("../views");
 
-router.get('/', (req, res, next) => {
-    res.send('got to GET /wiki/');
+// /wiki
+router.get("/", async (req, res, next) => {
+  try {
+    const pages = await Page.findAll();
+    res.send(main(pages));
+  } catch (error) { next(error) }
 });
 
-
-router.post('/', async (req, res, next) => {
-    const page = new Page({
-        title: req.body.title,
-        content: req.body.page_body
+// /wiki
+router.post("/", async (req, res, next) => {
+  try {
+    const [user, wasCreated] = await User.findOrCreate({
+      where: {
+        name: req.body.name,
+        email: req.body.email
+      }
     });
 
-    try {
-        await page.save();
-        res.redirect('/');
-    } catch (error) { next(error) }
+    const page = await Page.create(req.body);
+
+    page.setAuthor(user);
+
+    res.redirect("/wiki/" + page.slug);
+  } catch (error) { next(error) }
 });
 
-router.get('/add', (req, res, next) => {
-    res.send(addPage())
-})
+router.post("/:slug", async (req, res, next) => {
+  try {
+    const [updatedRowCount, updatedPages] = await Page.update(req.body, {
+      where: {
+        slug: req.params.slug
+      },
+      returning: true
+    });
+
+    res.redirect("/wiki/" + updatedPages[0].slug);
+  } catch (error) { next(error) }
+});
+
+router.get("/:slug/delete", async (req, res, next) => {
+  try {
+    await Page.destroy({
+      where: {
+        slug: req.params.slug
+      }
+    });
+
+    res.redirect("/wiki");
+  } catch (error) { next(error) }
+});
+
+// /wiki/add
+router.get("/add", (req, res) => {
+  res.send(addPage());
+});
+
+// /wiki/(dynamic value)
+router.get("/:slug", async (req, res, next) => {
+  try {
+    const page = await Page.findOne({
+      where: {
+        slug: req.params.slug
+      }
+    });
+    if (page === null) {
+      res.sendStatus(404);
+    } else {
+      const author = await page.getAuthor();
+      res.send(wikiPage(page, author));
+    }
+  } catch (error) { next(error) }
+});
+
+router.get("/:slug/edit", async (req, res, next) => {
+  try {
+    const page = await Page.findOne({
+      where: {
+        slug: req.params.slug
+      }
+    });
+
+    if (page === null) {
+      res.sendStatus(404);
+    } else {
+      const author = await page.getAuthor();
+      res.send(editPage(page, author));
+    }
+  } catch (error) { next(error) }
+});
 
 module.exports = router;
